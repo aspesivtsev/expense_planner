53c4d2d7d4f284b32bdd7db6f06dd2896ba6e983
Anatoly <anatoly@MacBook-Pro-Anatoly.local>
2020-10-28 00:38:25 +0300

    added trnsaction functionality start

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,11 +22,18 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New shirt', amount: 37.00, date: DateTime.now()),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 35,
         title: Text("Expense Planner"),
       ),
       body: Column(
