590db3d84e6650a534bce6e7ae2b10246dd0de5c
anatoly <anspesiv@yahoo.com>
2020-12-02 18:54:14 +0300

    started chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,9 +1,19 @@
+import 'package:expense_planner/models/transaction.dart';
 import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
-      return {'day': 'T', 'amount': 9.99};
+      final weekDay = DateTime.now().substract(
+        Duration(days: index),
+      );
+      print(weekDay);
+      return {'day': DateFormat.E(), 'amount': 9.99};
     });
   }
 
