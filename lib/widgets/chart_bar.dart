d697b2150eeac47629ecbb8927fbd4e08c5f6fc2
anatoly <anspesiv@yahoo.com>
2020-12-09 18:58:20 +0300

    working on chart

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -8,8 +8,19 @@ class Chartbar extends StatelessWidget {
   Chartbar(this.label, this.spendingAmount, this.speendingPctOfTotal)
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Column(children: [
+      Text('₽${spendingAmount.toStringAsFixed(0)}'),
+      SizedBox(height: 4,),
+      Container(height: 60, width: 10,child: 
+      Stack(children: [
+        Container(decoration: BoxDecoration(border: Border.all(color: Colors.grey, width: 1.0), color: Color.fromRGBO(220, 220, 220, 1),
+        borderRadius: BorderRadius.circular(10),),
+        ),
+        
+        FractionallySizedBox(heightFactor: speendingPctOfTotal, child: Container(decoration: BoxDecoration(color: Theme.of(context).primaryColor, borderRadius: BorderRadius.circular(10),)
+      ,),],),),
+      SizedBox(height: 4,),
+      Text(label),
+    ],);
   }
 }
