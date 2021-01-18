717fcc8d5b8ee1add982648a4867753df93a5d81
Anatoly <anatoly@MacBook-Pro-Anatoly.local>
2021-01-19 01:09:35 +0300

    encreasing performance

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -6,7 +6,7 @@ class ChartBar extends StatelessWidget {
   final double spendingAmount;
   final double speendingPctOfTotal;
 
-  ChartBar(
+  const ChartBar(
       this.label, this.numDate, this.spendingAmount, this.speendingPctOfTotal);
 
   @override
